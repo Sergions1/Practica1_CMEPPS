c1b2e6633f6342291667bab7f4510177c3774db9
Sergions1 <sergionunezsierra1@gmail.com>
2025-09-24 18:52:40 +0200

    3

--- src/tutorial/HolaMundo.java
+++ src/tutorial/HolaMundo.java
@@ -4,5 +4,6 @@ public class HolaMundo {
 	public static void main(String[] ares) {
 		System.out.println( "Hola CMEPPS ");
 		System.out.println( "Primera Rama");
+		System.out.println( "segundo push de la primera rama");
 	}
 }
